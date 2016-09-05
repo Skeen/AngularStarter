7162c53cd953fcac3e8ec876dec9c557f69582d3
Emil 'Skeen' Madsen <sovende@gmail.com>
2016-09-05 18:23:45 +0200

    Clean up.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,3 @@
-import './polyfills';
-
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
